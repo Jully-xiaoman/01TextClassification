1e5a272547c42d936c49599b6fdaf94d874341c4
CrazyBigBanana <95234746+Jully-xiaoman@users.noreply.github.com>
2026-03-24 20:16:06 +0800

    去除数据逻辑，以接口形式保留

--- train_model.py
+++ train_model.py
@@ -1,106 +1,13 @@
-import pandas as pd
 import torch
-from torch.utils.data import Dataset, DataLoader
-from transformers import AutoTokenizer
+import json
 from transformers import BertForSequenceClassification
-# pandas是表格数据结构，很像excel表格
+from data_module import create_datasets_and_loaders
 
-# 1.读取txt文件并转换为df；
-def read_txt_to_df(file_path):
-    rows = []
-    with open(file_path, "r", encoding="utf-8") as f:
-        for line in f:
-            # strip():去除首尾巴的换行和空格符
-            # split("_!_"):按括号内字符分割，变成一个列表
-            parts = line.strip().split("_!_")
-            if len(parts) == 5:
-                rows.append(parts)
+with open("args.json", "r", encoding="utf-8") as f:
+    config = json.load(f)
 
-    # columns是指给每一列取个列名
-    df = pd.DataFrame(rows, columns=[
-        "news_id", "category_code", "category_name", "title", "keywords"
-    ])
-    return df
-
-# 2.读取train_df,dev_df,test_df;
-train_df = read_txt_to_df(r"./RawData/train_3k.txt")
-dev_df = read_txt_to_df(r"./RawData/dev_1k.txt")
-test_df = read_txt_to_df(r"./RawData/test_1k.txt")
-
-# print(train_df.head())
-# print(dev_df.head())
-# print(test_df.head())
-
-# 3.建立标签映射;
-label_names = sorted(train_df["category_name"].unique())
-# 字典形式
-label2id = {label: idx for idx, label in enumerate(label_names)}
-id2label = {idx: label for label, idx in label2id.items()}
-
-# print("标签映射：")
-# print(label2id)
-# print("类别数量：", len(label2id))
-
-# 4. 提取 texts 和 labels
-train_texts = train_df["title"].tolist()
-# train_texts = train_df["title"] 后是pandas的series，带有行号。
-# 必须要转换为普通列表模型才能接收。
-train_labels = train_df["category_name"].map(label2id).tolist()
-
-dev_texts = dev_df["title"].tolist()
-dev_labels = dev_df["category_name"].map(label2id).tolist()
-
-test_texts = test_df["title"].tolist()
-test_labels = test_df["category_name"].map(label2id).tolist()
-
-# 输出查看
-# print("label2id:", label2id)
-# print("train_texts前3条:", train_texts[:3])
-# print("train_labels前3条:", train_labels[:3])
-
-# 5.加载 tokenizer
-# 把 bert-base-chinese 这个模型配套的 tokenizer 加载出来，并命名为 tokenizer。
-tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese")
-
-# 6.写Dataset类：返回单条样本的处理结果
-class NewsDataset(Dataset):
-    def __init__(self, texts, labels, tokenizer, max_length=32):
-        self.texts = texts
-        self.labels = labels
-        self.tokenizer = tokenizer
-        self.max_length = max_length
-
-    # 输出数据集的size
-    def __len__(self):
-        return len(self.texts)
-
-    # 按照编号获取数据的标准格式
-    def __getitem__(self, idx):
-        text = self.texts[idx]
-        label = self.labels[idx]
-
-        # 每次处理一条数据
-        encoding = self.tokenizer(
-            text,
-            padding="max_length",
-            truncation=True,
-            max_length=self.max_length,
-            return_tensors="pt"
-        )
-
-        # 单条数据处理后的固定输出格式
-        return {
-            "input_ids": encoding["input_ids"].squeeze(0),
-            "attention_mask": encoding["attention_mask"].squeeze(0),
-            "labels": torch.tensor(label, dtype=torch.long)
-        }
-
-train_dataset = NewsDataset(train_texts, train_labels, tokenizer, max_length=32)
-train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
-dev_dataset = NewsDataset(dev_texts, dev_labels, tokenizer, max_length=32)
-dev_dataloader = DataLoader(dev_dataset, batch_size=8, shuffle=False)
-test_dataset = NewsDataset(test_texts, test_labels, tokenizer, max_length=32)
-test_dataloader = DataLoader(test_dataset, batch_size=8, shuffle=False)
+# 1.数据接口
+train_dataloader, dev_dataloader, test_dataloader, label2id, id2label = create_datasets_and_loaders(config)
 
 # 7.加载模型，准备优化器
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
